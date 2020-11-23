e165b4808161d9ea67631c4f88562bf255529384
Aevan Candelaria <aevan.candelaria@gmail.com>
2020-11-23 14:05:38 +0800

    refactored index.ejs and deleted form partials

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
 
